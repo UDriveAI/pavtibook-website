757bf71b137051976d604a2f652bf5b8742ed7ac
UDriveAI <nik.pranay1@outlook.com>
2026-09-17 21:35:53 +0530

    Add Google AdSense site verification script to root layout head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -191,6 +191,11 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <head>
         <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5607512862337979"
+          crossOrigin="anonymous"
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(softwareAppSchema) }}
         />
